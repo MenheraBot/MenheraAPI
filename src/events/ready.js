a7b705efbface4a745231d1bec564b26ffc0ac60
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-15 20:37:36 -0300

    Dando os carguin a cada 12 horas

--- src/events/ready.js
+++ src/events/ready.js
@@ -24,6 +24,45 @@ module.exports = class {
 
         setInterval(() => {
             require('../structures/TopFamilyChecks').run()
-        }, 1000 * 60 * 60 * 24)
+            const server = this.client.guilds.cache.get("717061688460967988")
+
+            const roleApolo = server.roles.cache.get('765069063146962995')
+            const roleLoki = server.roles.cache.get('765069110018703371')
+            const roleAres = server.roles.cache.get('765069139885948928')
+            const roleSoma = server.roles.cache.get('765069167363096616')
+            const roleFreya = server.roles.cache.get('765069003440914443')
+
+            const apolo = await this.client.database.familia.findById("Apolo")
+            const loki = await this.client.database.familia.findById("Loki")
+            const ares = await this.client.database.familia.findById("Ares")
+            const soma = await this.client.database.familia.findById("Soma")
+            const freya = await this.client.database.familia.findById("Freya")
+
+            apolo.members.forEach(member => {
+                if (message.guild.members.cache.has(member)) {
+                    message.guild.members.cache.get(member).roles.add(roleApolo)
+                }
+            })
+            loki.members.forEach(member => {
+                if (message.guild.members.cache.has(member)) {
+                    message.guild.members.cache.get(member).roles.add(roleLoki)
+                }
+            })
+            ares.members.forEach(member => {
+                if (message.guild.members.cache.has(member)) {
+                    message.guild.members.cache.get(member).roles.add(roleAres)
+                }
+            })
+            soma.members.forEach(member => {
+                if (message.guild.members.cache.has(member)) {
+                    message.guild.members.cache.get(member).roles.add(roleSoma)
+                }
+            })
+            freya.members.forEach(member => {
+                if (message.guild.members.cache.has(member)) {
+                    message.guild.members.cache.get(member).roles.add(roleFreya)
+                }
+            })
+        }, 1000 * 60 * 60 * 12)
     }
 }
