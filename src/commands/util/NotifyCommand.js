18c2da8317e02b250f6cb8a9a053564968faa7a7
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-23 16:44:35 -0300

    Notify Command

---
+++ src/commands/util/NotifyCommand.js
@@ -0,0 +1,22 @@
+const Command = require("../../structures/command")
+module.exports = class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "notify",
+            category: "util",
+            cooldown: 5
+        })
+    }
+    async run(message, args) {
+
+        const role = await this.client.guilds.cache.get('717061688460967988').roles.cache.get('755593580285788280')
+       
+        if(message.member.roles.cache.has(role.id)) {
+            message.channel.send("Como desejar! Você não será mais notificado das atualizações da Menhera!")
+            message.member.roles.remove(role, "Comando Notify")
+        } else {
+            message.channel.send("Feitoria! Você será notificado das atualizações da Menhera")
+            message.member.roles.add(role, "Comando Notify")
+        }
+    }
+}
