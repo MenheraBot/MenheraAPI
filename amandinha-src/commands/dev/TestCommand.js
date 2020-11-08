5c16534cb50a0b1265c56ca3c8e748d00a2dc68e
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-11-08 18:14:37 -0300

    :tada: | Iniciando testes pra canvas e jimp

--- amandinha-src/commands/dev/TestCommand.js
+++ amandinha-src/commands/dev/TestCommand.js
@@ -1,5 +1,4 @@
 const Command = require("../../structures/command")
-const test = require("../../structures/TopFamilyChecks")
 module.exports = class TestCommand extends Command {
     constructor(client) {
         super(client, {
@@ -9,8 +8,8 @@ module.exports = class TestCommand extends Command {
             OnlyDevs: true
         })
     }
-
+    
     async run(message, args) {
-     test.run(this.client)
+    // test.run(this.client)
     }
 }
