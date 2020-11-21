6d7296a96338a5e41ceaee33d070cde541135aba
Luxanna <CommandPlayermine@gmail.com>
2020-11-21 04:27:18 -0300

    :star: | Changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
     args: "start"
   }, {
     name: "Menhera Dashboard",
-    cwd: "./packages/Amandinha",
+    cwd: "./packages/Dashboard",
     script: "npm",
     args: "start"
   }],
