6af3528ec4ec736f14362b5c3ce2461496a34ae0
Luxanna <CommandPlayermine@gmail.com>
2020-11-24 22:12:46 -0300

    :boom: | Start Using TypeScript
    Co-authored-by: Tsugami <yslanramos0@gmail.com>

--- packages/API/routes/404.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const router = express.Router()
-
-router.get('/', (req, res) => {
-    res.status(404).send(`<h1 style='font-size:3000%; color:red'><center>404</center></h1>`)
-})
-
-module.exports = router;
