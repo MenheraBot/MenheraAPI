e6ba9a755cb6b0a29858625b76253203c9bcf01b
Yslan Ramos <yslan3000@gmail.com>
2020-11-27 21:12:53 -0300

    :bug: | fix sendings webhook
    the webhook routes were returning 'ok' when they did not send the message.

--- packages/API/src/controllers/BotController.ts
+++ packages/API/src/controllers/BotController.ts
@@ -1,25 +1,23 @@
 import { Request, Response } from 'express';
-import { status } from '../util/sendMessage';
+import { responseStatus } from '../util/sendMessage';
 import { Colors } from '../util/interfaces';
 
 export default class BotController {
   public static async ready(_req: Request, res: Response): Promise<Response> {
-    await status({
+    return responseStatus(res, {
       title: 'Menhera is ON',
       description: 'Menhera voltou de férias, e já está online respondendo comandos!',
       color: Colors.Green,
       timestamp: new Date(),
     });
-    return res.sendStatus(200);
   }
 
   public static async down(_req: Request, res: Response): Promise<Response> {
-    await status({
+    return responseStatus(res, {
       title: 'DOWN',
       description: 'ESSA NÃO!!!\nMenhera foi de base!',
       color: Colors.Red,
       timestamp: new Date(),
     });
-    return res.sendStatus(200);
   }
 }
--- packages/API/src/util/sendMessage.ts
+++ packages/API/src/util/sendMessage.ts
@@ -1,20 +1,21 @@
 import executeWebhook, { EmbedOptions } from '@menhera-tools/execute-webhook';
+import { Response } from 'express';
 import { Colors } from './interfaces';
 
-export async function status(embed: EmbedOptions) {
-  executeWebhook(process.env.WEBHOOK_ID_STATUS, process.env.WEBHOOK_TOKEN_STATUS, {
+export async function status(embed: EmbedOptions): Promise<void> {
+  await executeWebhook(process.env.WEBHOOK_ID_STATUS, process.env.WEBHOOK_TOKEN_STATUS, {
     embeds: [embed],
   });
 }
 
-export async function github(stats: number, message: string) {
+export async function github(stats: number, message: string): Promise<void> {
   const isError = stats > 399;
   const color = isError ? Colors.Red : Colors.Green;
   const thumbnail = isError
     ? 'https://i.imgur.com/t94XkgG.png'
     : 'https://cdn140.picsart.com/283880390029211.png?type=webp&to=min&r=640';
 
-  executeWebhook(process.env.WEBHOOK_ID_GITHUB, process.env.WEBHOOK_TOKEN_GITHUB, {
+  await executeWebhook(process.env.WEBHOOK_ID_GITHUB, process.env.WEBHOOK_TOKEN_GITHUB, {
     embeds: [{
       title: String(stats),
       description: message,
@@ -23,4 +24,21 @@ export async function github(stats: number, message: string) {
       thumbnail: { url: thumbnail },
     }],
   });
+}
+
+export function responseWebhook(res: Response, promise: Promise<any>): Promise<Response> {
+  return promise
+    .then(() => res.sendStatus(201))
+    .catch((error) => {
+      console.error(error);
+      return res.status(502).json({ message: 'Could not send webhook' });
+    });
+}
+
+export function responseStatus(res: Response, embed: EmbedOptions): Promise<Response> {
+  return responseWebhook(res, status(embed));
+}
+
+export function responseGithub(res: Response, stats: number, message: string): Promise<Response> {
+  return responseWebhook(res, github(stats, message));
 }
