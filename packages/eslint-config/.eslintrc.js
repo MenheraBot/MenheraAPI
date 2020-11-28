425dc301905e29d4a5adc850835dd9480ce77123
Yslan Ramos <yslan3000@gmail.com>
2020-11-28 04:47:34 -0300

    :clown_face: | desable react eslint config
    does not work in jsx files

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,49 +1,47 @@
 module.exports = {
   env: {
     es2021: true,
+    //  es2020: true,
     node: true,
+    // browser: true,
   },
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'prettier',
+    // 'plugin:react/recommended',
+    // 'prettier/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    // ecmaFeatures: {
+    //   jsx: true,
+    // },
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', /* 'react' */, 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'import/extensions': ['error', 'never'],
     'import/no-extraneous-dependencies': 'off',
-    "@typescript-eslint/type-annotation-spacing": [
-      "error",
+    '@typescript-eslint/type-annotation-spacing': [
+      'error',
       {
-        "after": true,
-        "before": false
-      }
-    ]
+        after: true,
+        before: false,
+      },
+    ],
   },
 
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': [
-        '.d.ts',
-        '.ts',
-      ],
+      '@typescript-eslint/parser': ['.ts'],
     },
     'import/resolver': {
       node: {
-        extensions: [
-          '.js',
-          '.ts',
-        ],
+        extensions: ['.ts'],
       },
       typescript: {},
     },
