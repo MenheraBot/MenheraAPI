c3fe6d8573661705f1717c9acdf52f0f3fc8e871
Luxanna <CommandPlayermine@gmail.com>
2021-06-08 21:19:22 -0300

    :alembic: | This is a test

--- packages/API/src/controllers/StatusController.ts
+++ packages/API/src/controllers/StatusController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export default class StatusController {
   public static async status(req: Request, res: Response): Promise<Response> {
-    console.log(req.body);
+    console.log(req.body.maintenance.maintenance_updates);
 
     return res.sendStatus(201);
   }
