6d11e8a86877a6a771b0d8dde48ebb829594f353
Luxanna <CommandPlayermine@gmail.com>
2021-06-09 00:26:08 -0300

    :star: | Finish i guess

--- packages/API/src/controllers/StatusController.ts
+++ packages/API/src/controllers/StatusController.ts
@@ -1,7 +1,6 @@
 import { EmbedOptions } from '@menhera-tools/execute-webhook';
 import { Request, Response } from 'express';
 import moment from 'moment';
-import util from 'util';
 import SendStatus from '../util/statusMessage';
 import { Colors, Titles } from '../util/constants';
 
@@ -10,7 +9,6 @@ export default class StatusController {
   public static async status(req: Request, res: Response): Promise<Response> {
     const { body } = req;
 
-    console.log(util.inspect(body, false, null, true));
     if (body.component) StatusController.SendComponent(body);
     if (body.incident) StatusController.SendIncident(body);
     if (body.maintenance) StatusController.SendMaintenance(body);
@@ -42,11 +40,13 @@ export default class StatusController {
   }
 
   public static async SendIncident(body): Promise<void> {
+    const index = body.incident.incident_updates.findIndex(i => i.status === body.incident.status);
+
     const embed: EmbedOptions = {
       color: Colors[body.incident.status],
       title: body.incident.name,
-      description: `${Titles[body.incident.status]}\n${body.incident.incident_updates[
-        body.incident.incident_updates.length - 1
+      description: `**${Titles[body.incident.status]}**\n\n${body.incident.incident_updates[
+        index
       ].body
         .slice(0, -4)
         .slice(3)}`,
@@ -59,14 +59,15 @@ export default class StatusController {
   }
 
   public static async SendMaintenance(body): Promise<void> {
+    const index = body.incident.maintenance_updates.findIndex(
+      i => i.status === body.maintenance.status
+    );
     const embed: EmbedOptions = {
       color: Colors[body.maintenance.status],
       title: body.maintenance.name,
-      description: `${Titles[body.maintenance.status]}\n${body.maintenance.maintenance_updates[
-        body.maintenance.maintenance_updates.length - 1
-      ].body
-        .slice(0, -4)
-        .slice(3)}`,
+      description: `**${
+        Titles[body.maintenance.status]
+      }**\n\n${body.maintenance.maintenance_updates[index].body.slice(0, -4).slice(3)}`,
       footer: {
         text: moment.utc(Date.now()).format('L [às] LTS'),
       },
