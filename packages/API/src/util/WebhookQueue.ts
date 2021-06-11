28f7650fb80ed2f7f0189c8b4f07a3302726da8c
Luxanna <CommandPlayermine@gmail.com>
2021-06-11 11:30:12 -0300

    :star: | Add embed queue

---
+++ packages/API/src/util/WebhookQueue.ts
@@ -0,0 +1,49 @@
+import executeWebhook, { EmbedOptions } from '@menhera-tools/execute-webhook';
+import { StatusRole } from './constants';
+
+let queue: EmbedOptions[] = [];
+let debounce = false;
+let timeout;
+
+async function sendWebhook(arr): Promise<void> {
+  if (arr.length <= 10) {
+    await executeWebhook(process.env.WEBHOOK_ID_STATUS, process.env.WEBHOOK_TOKEN_STATUS, {
+      content: StatusRole,
+      embeds: queue,
+    });
+  } else {
+    const firstArray = arr.slice(0, 10);
+    const secondArray = arr.slice(11, arr.length - 1);
+    await executeWebhook(process.env.WEBHOOK_ID_STATUS, process.env.WEBHOOK_TOKEN_STATUS, {
+      content: StatusRole,
+      embeds: firstArray,
+    });
+    await executeWebhook(process.env.WEBHOOK_ID_STATUS, process.env.WEBHOOK_TOKEN_STATUS, {
+      content: StatusRole,
+      embeds: secondArray,
+    });
+  }
+}
+
+async function startCounting() {
+  if (!debounce) {
+    debounce = true;
+    timeout = setTimeout(() => {
+      sendWebhook(queue);
+      debounce = false;
+      queue = [];
+    }, 10000);
+  } else {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      sendWebhook(queue);
+      debounce = false;
+      queue = [];
+    }, 10000);
+  }
+}
+
+export default function Queue(embed: EmbedOptions): void {
+  queue.push(embed);
+  startCounting();
+}
