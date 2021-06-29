0fc08eb0eb4729a7e2e0187048b1fe4a9ff25429
Luxanna <CommandPlayermine@gmail.com>
2021-06-29 17:55:55 -0300

    :sparkles: | Add https support

--- packages/API/src/server.ts
+++ packages/API/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import Http from 'http';
+import Https from 'https';
+import fs from 'fs';
 import logger from '@menhera-tools/logger';
 import { config } from 'dotenv';
 import path from 'path';
@@ -13,8 +15,21 @@ import ErrorHandler from './middlewares/ErrorHandler';
 config({ path: path.resolve(path.join(__dirname, '..', '.env')) });
 
 const app = express();
-const server = Http.createServer(app);
 
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/vps.menherabot.xyz/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/vps.menherabot.xyz/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/vps.menherabot.xyz/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca,
+};
+
+const httpServer = Http.createServer(app);
+const httpsServer = Https.createServer(credentials, app);
+
+app.use(express.static(__dirname, { dotfiles: 'allow' }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +39,10 @@ app.use('/api', routes);
 app.use(NotFound);
 app.use(ErrorHandler);
 
-server.listen(process.env.PORT, () => {
-  logger.info(`[API] Server started on port ${process.env.PORT}`);
+httpServer.listen(80, () => {
+  logger.info('[API] HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+  logger.info('[API] HTTPS Server running on port 443');
 });
