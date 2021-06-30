9d95e515b46a6e133f941fd56f84a47607d440ac
Luxanna <CommandPlayermine@gmail.com>
2021-06-30 17:20:10 -0300

    :sta: | sim

--- packages/API/src/server.ts
+++ packages/API/src/server.ts
@@ -13,8 +13,7 @@ import ErrorHandler from './middlewares/ErrorHandler';
 config({ path: path.resolve(path.join(__dirname, '..', '.env')) });
 
 const app = express();
-
-const httpServer = Http.createServer(app);
+const server = Http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +24,6 @@ app.use('/api', routes);
 app.use(NotFound);
 app.use(ErrorHandler);
 
-httpServer.listen(80, () => {
-  logger.info('[API] HTTP Server running on port 80');
+server.listen(process.env.PORT, () => {
+  logger.info(`[API] Server started on port ${process.env.PORT}`);
 });
