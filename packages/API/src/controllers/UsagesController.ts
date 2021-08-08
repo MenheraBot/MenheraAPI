7d192f4c929d610286e050df13ee64623d580b83
Luxanna <CommandPlayermine@gmail.com>
2021-08-08 02:59:04 -0300

    fix: Fix some problems

--- packages/API/src/controllers/UsagesController.ts
+++ packages/API/src/controllers/UsagesController.ts
@@ -25,9 +25,7 @@ export default class UsagesController {
       userId,
     ]);
 
-    console.log(commandsExecuted)
-
-    if (!commandsExecuted?.rows[0] || commandsExecuted.rows[0]?.count === '0')
+    if (!commandsExecuted?.rows[0] || commandsExecuted.rows[0]?.count == 0)
       return res.status(404).send('Este usuário não exsite em meu banco de dados');
     const allCommands = await pool.query(
       'SELECT cmds.name, COUNT(cmds.name) FROM uses INNER JOIN cmds ON uses.cmd_id = cmds.id WHERE user_id = $1 GROUP BY cmds.name ORDER BY count DESC LIMIT 10',
