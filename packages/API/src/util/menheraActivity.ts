7556c53fd2a88a7be7b3aaf9a3f64635cb120178
Luxanna <CommandPlayermine@gmail.com>
2021-08-10 22:05:18 -0300

    feat: New Actiity

--- packages/API/src/util/menheraActivity.ts
+++ packages/API/src/util/menheraActivity.ts
@@ -5,30 +5,8 @@ export default class Activities {
 
   private static getDefaultActivities(): Array<Activity> {
     return [
-      { name: '🚨 | Meu servidor de suporte m!suporte', type: 'LISTENING' },
-      { name: '🎇 | Já votou em mim hoje? m!votar', type: 'PLAYING' },
-      { name: '🎮 | Caçe demônios com XANDÃO. m!caçar', type: 'PLAYING' },
       {
-        name: '🔮 | Tem ideia de um comando interessante? Use m!sugerir',
-        type: 'PLAYING',
-      },
-      {
-        name: '💌 | Dificuldade com um comando? Use m!help comando',
-        type: 'PLAYING',
-      },
-      { name: '🐛 | Encontrou um bug? Reporte com m!bug', type: 'PLAYING' },
-      {
-        name: '❓ | Duvidas? Entre em meu servidor de suporte m!suporte',
-        type: 'PLAYING',
-      },
-      {
-        name: '🔔 | Fique por dentro das minhas novidades em meu servidor de suporte',
-        type: 'PLAYING',
-      },
-      { name: '🎲 | Sabia que eu tenho um rpg? m!help', type: 'PLAYING' },
-      {
-        name:
-          '🎲 | Registre-se um aventureiro com m!register, e vá para aventuras na dungeon com m!dungeon',
+        name: '⚠️ | COMANDOS ANTIGOS PARARAM DE FUNCIONAR! DIGITE / PARA USAR OS NOVOS COMANDOS',
         type: 'PLAYING',
       },
     ];
