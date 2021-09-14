82cef0430a3045e2b21124d5c91f633b78767a89
Luxanna <CommandPlayermine@gmail.com>
2021-09-13 23:27:32 -0300

    feat(Add Hunt)

--- packages/API/src/api/database/MongoDB.ts
+++ packages/API/src/api/database/MongoDB.ts
@@ -1,10 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const initiatedAt = Date.now();
 
