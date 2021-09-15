5502b241d0e7bc94289a492186139c1867d67bf3
Luxanna <CommandPlayermine@gmail.com>
2021-09-15 00:28:11 -0300

    fix: Try to fix Suggest bug

--- packages/Amandinha/src/index.ts
+++ packages/Amandinha/src/index.ts
@@ -1,7 +1,5 @@
 /* eslint-disable no-console */
-import startServer from './server/index';
 import Client from './client';
-
 
 const client = new Client({
   partials: ['MESSAGE', 'REACTION'],
@@ -11,12 +9,17 @@ const client = new Client({
 client.loadCommands(`${__dirname}/commands`);
 client.loadEvents(`${__dirname}/events`);
 client
-.login(process.env.TOKEN as string)
-.then(() => {
-  console.info('Amandinha ta voando alto!');
-  startServer(client);
-})
-.catch(e =>
-  console.error(`EITA PORRA, DEU MERDA AO TENTAR SE CONECTAR NO DISCORD! ${e.message}`)
+  .login(process.env.TOKEN as string)
+  .then(() => {
+    console.info('Amandinha ta voando alto!');
+    /*
+     * ============ ONLY UNCOMMENT THIS TO START HTTP SERVER TO INTERACTIONS
+     *
+     * import startServer from './server/index';
+     * startServer(client);
+     *
+     */
+  })
+  .catch(e =>
+    console.error(`EITA PORRA, DEU MERDA AO TENTAR SE CONECTAR NO DISCORD! ${e.message}`)
   );
-  
