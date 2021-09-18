a23f3dc064c4f0bf053bf28ad4997c10e130da1a
Luxanna <CommandPlayermine@gmail.com>
2021-09-18 04:36:44 -0300

    feat(Auth): Change authorization way

--- packages/API/src/api/middlewares/isAuthorized.ts
+++ packages/API/src/api/middlewares/isAuthorized.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): Response | void => {
-  const { token } = req.headers;
+  const { authorization } = req.headers;
 
-  if (!token || token !== process.env.API_TOKEN) {
+  if (!authorization || authorization !== process.env.API_TOKEN) {
     return res.status(401).send({ message: 'Only the Menhera Client can access that!' });
   }
 
