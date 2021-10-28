ec00fd5e89842c56b93bd92ff8773464321491c1
Luxanna <CommandPlayermine@gmail.com>
2021-10-28 03:49:45 -0300

    a

--- packages/API/src/api/controllers/usages.controllers.ts
+++ packages/API/src/api/controllers/usages.controllers.ts
@@ -15,7 +15,7 @@ export default class UsagesController {
   }
 
   static async getInactiveUsers(req: Request, res: Response): Promise<Response> {
-    const { users } = req.body;
+    const { users } = req.body.data;
 
     console.log(users);
 
