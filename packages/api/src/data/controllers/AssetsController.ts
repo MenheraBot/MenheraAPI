009e7e6befc8830bdf508a7725e331764412e9d2
Luxanna <CommandPlayermine@gmail.com>
2022-05-22 06:30:25 -0300

    feat: add menhera cdn

--- packages/api/src/data/controllers/AssetsController.ts
+++ packages/api/src/data/controllers/AssetsController.ts
@@ -1,25 +1,16 @@
 import { Request, Response } from 'express';
-import urls from '../util/AssetsURL';
+import CdnManager, { AssetsLimit } from '../util/CdnManager';
 
 export default class AssetsController {
   public static async getImageUrl(req: Request, res: Response): Promise<Response> {
-    const { type } = req.params;
+    const type = req.params.type as keyof AssetsLimit;
 
     if (!type) return res.sendStatus(400);
 
-    const url = AssetsController.getAssetBasedOnTheType(type);
+    const url = CdnManager.getInstance().getLink(type);
 
     if (url === 'NO-LINKS') return res.sendStatus(404);
 
     return res.json({ url });
-  }
-
-  private static getAssetBasedOnTheType(type: string): string {
-    const allLinks = urls[type];
-
-    if (!allLinks) return 'NO-LINKS';
-    const randomLink = allLinks[Math.floor(Math.random() * allLinks.length)];
-
-    return randomLink;
   }
 }
