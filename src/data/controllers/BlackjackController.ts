b2cc2c0846d392b1e8421bba3830b0d44f8f9f8d
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-10-28 15:07:23 -0300

    feat: coinflip and blackjack have its own separate table

--- src/data/controllers/BlackjackController.ts
+++ src/data/controllers/BlackjackController.ts
@@ -4,18 +4,21 @@ import { getBlackJackStats, getTopBlackjack, postBlackJackGame } from '../databa
 export default class BlackJackController {
   public static async getUserInfo(req: Request, res: Response): Promise<Response> {
     const { userId } = req.body;
+
     if (!userId) return res.sendStatus(400);
+
     const result = await getBlackJackStats(userId);
 
-    if (!result) return res.sendStatus(400);
+    if (!result) return res.sendStatus(404);
 
-    const playedGames = result.bj_wins + result.bj_loses;
+    const playedGames = result.lost_games + result.won_games;
 
     if (playedGames === 0) return res.status(200).send({ error: true });
-    const lostGames = result.bj_loses;
-    const winGames = result.bj_wins;
-    const winMoney = result.bj_win_money;
-    const lostMoney = result.bj_lose_money;
+
+    const lostGames = result.lost_games;
+    const winGames = result.won_games;
+    const winMoney = result.earn_money;
+    const lostMoney = result.lost_money;
     const winPorcentage = ((winGames / playedGames) * 100).toFixed(2) || 0;
     const lostPorcentage = ((lostGames / playedGames) * 100).toFixed(2) || 0;
     const returnObject = {
