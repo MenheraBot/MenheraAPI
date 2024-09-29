f474cfab22031f2ddf539d280939b5b4882d9928
ySnoopyDogy <CommandPlayermine@gmail.com>
2024-09-29 04:23:24 -0300

    feat: print route in log

--- src/data/middlewares/ErrorHandler.ts
+++ src/data/middlewares/ErrorHandler.ts
@@ -1,14 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-export default (
-  error: Error,
-  _req: Request,
-  res: Response,
-  next: NextFunction
-): Response | void => {
+export default (error: Error, req: Request, res: Response, next: NextFunction): Response | void => {
   if (!error) return next();
 
-  console.error(new Date().toISOString(), error.message);
+  console.error(`${new Date().toISOString()} [${req.method} ${req.url}]`, error.message);
 
   return res.status(500).send({ message: 'An error occurred trying to process your request' });
 };
